66345b7f545e337d8b2440ad37e92fa11206a344
thesaquib <abdulsaquib150@gmail.com>
2025-04-28 02:59:39 +0530

    Top Tier Talent AI website, content, images

---
+++ toptire/js/custom.js
@@ -0,0 +1,28 @@
+
+  (function ($) {
+  
+  "use strict";
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+    var el = $(this).attr('href');
+    var elWrapped = $(el);
+    var header_height = $('.navbar').height();
+
+    scrollToDiv(elWrapped,header_height);
+    return false;
+
+    function scrollToDiv(element,navheight){
+      var offset = element.offset();
+      var offsetTop = offset.top;
+      var totalScroll = offsetTop-navheight;
+
+      $('body,html').animate({
+      scrollTop: totalScroll
+      }, 300);
+    }
+});
+    
+  })(window.jQuery);
+
+
